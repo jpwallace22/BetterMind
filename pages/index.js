a27ff5e16949a0bdfbe93dc7f791aca64e4563bf
jpwallace22 <93415734+jpwallace22@users.noreply.github.com>
2022-01-23 09:36:08 -0800

    h2

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,9 @@ export default function Home() {
 
       <main className="homepage grid">
         <div className="hero-content px-1">
-          <h2 className="hero">Come see us you son of a bitch!</h2>
+          <h2 className="hero">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit
+          </h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
