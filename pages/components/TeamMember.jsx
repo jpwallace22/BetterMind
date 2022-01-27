ec6e8960f2f57913393d1e7fc349b9ae462b621d
jpwallace22 <93415734+jpwallace22@users.noreply.github.com>
2022-01-27 15:10:56 -0800

    team images added js media query and priority loading

--- pages/components/TeamMember.jsx
+++ pages/components/TeamMember.jsx
@@ -2,15 +2,30 @@ import React from "react";
 import Image from "next/image";
 
 function TeamMember({ image, alt, name, title, description }) {
+  const handleClick = (target) => {
+    if (window.matchMedia("(max-width: 900px)").matches) {
+      console.log(window.matchMedia("(max-width: 900px)"));
+      const currentlyOpened = document.querySelector(".show-info");
+      if (currentlyOpened && currentlyOpened !== target.closest("li")) {
+        currentlyOpened.classList.remove("show-info");
+      }
+      target.closest("li").classList.toggle("show-info");
+    }
+    return;
+  };
+
   return (
-    <li className="hex-grid__item">
+    <li
+      className="hex-grid__item"
+      onClick={({ target }) => handleClick(target)}
+    >
       <div className="hex-grid__content">
         <div className="about">
           <h3>{name}</h3>
           <h4>{title}</h4>
           <p>{description}</p>
         </div>
-        <img src={image} alt={alt} layout="fill" />
+        <Image priority={true} src={image} alt={alt} layout="fill" />
       </div>
     </li>
   );
