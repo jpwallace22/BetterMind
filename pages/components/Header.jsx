deb69216a82af229245fccee2e72735061edbab3
jpwallace22 <93415734+jpwallace22@users.noreply.github.com>
2022-01-28 12:57:19 -0800

    subscribe function added subscribe styling and functions. need to add mailing list and the functions for acutaly subscribing

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
-import { FaTimes, FaGripLines } from "react-icons/fa";
+import { FaTimes, FaGripLines, FaMailBulk } from "react-icons/fa";
 import DropdownItem from "./DropdownItem";
 
 function Header() {
   const router = useRouter();
   const [isActive, setIsActive] = useState(false);
+  const [subscribeOpen, setSubscribeOpen] = useState(false);
   let ref = useRef(null);
+  let sub = useRef(null);
 
   //closes all dropdown menus
   const closeAllMenus = () => {
@@ -21,10 +23,33 @@ function Header() {
     closeAllMenus();
   };
 
+  //mouse over for email subscription
+  const handleMouseOver = (e) => {
+    if (!e.target.disabled) {
+      const rect = e.target.getBoundingClientRect();
+      const mouseX = ((e.clientX - rect.left) / rect.width) * 100;
+      const mouseY = ((e.clientY - rect.bottom) / rect.height) * 100;
+      e.target.style.background = `radial-gradient(
+              circle at ${mouseX}% ${mouseY + 100}%,
+              rgba(40, 190, 172, 1),
+              rgba(24, 96, 139, 1)
+            )`;
+    }
+  };
+
   //closes menus when clicking outside of the header (for good ux)
   const handleClickOutside = (event) => {
     if (ref.current && !ref.current.contains(event.target)) {
       closeAllMenus();
+    }
+    if (sub.current && !sub.current.contains(event.target)) {
+      setSubscribeOpen(false);
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Escape") {
+      setSubscribeOpen(false);
     }
   };
 
@@ -41,8 +66,10 @@ function Header() {
   //add the click outside of component event listener
   useEffect(() => {
     document.addEventListener("click", handleClickOutside, true);
+    document.addEventListener("keydown", handleKeyPress);
     return () => {
       document.removeEventListener("click", handleClickOutside, true);
+      document.removeEventListener("keypress", handleKeyPress);
     };
   });
 
@@ -119,10 +146,8 @@ function Header() {
                 <a>Contact</a>
               </Link>
             </li>
-            <li>
-              <Link href="/">
-                <a>Subscribe</a>
-              </Link>
+            <li onClick={() => setSubscribeOpen(true)}>
+              <span>Subscribe</span>
             </li>
           </ul>
           {!isActive && (
@@ -140,17 +165,42 @@ function Header() {
         </nav>
       </div>
       {isActive && <div className="overlay"></div>}
-      <div className="subscribe-wrapper">
-        <div className="subscribe card">
-          <form>
-            <label htmlFor="subscribe">
-              <h2>Enter your email address</h2>
-            </label>
-            <input type="email" name="subscribe" id="subscribe" />
-            <button className="btn-primary">Subscribe</button>
-          </form>
+      {subscribeOpen && (
+        <div className="subscribe-wrapper">
+          <div className="subscribe card" ref={sub}>
+            <form>
+              <FaMailBulk size={90} color="var(--light-blue)" />
+              <label htmlFor="subscribe">
+                <h2>Enter your email address</h2>
+              </label>
+              <p>
+                And we’ll keep you up to date with the most pertinent mental
+                health information on the planet!
+              </p>
+              <input
+                type="email"
+                name="subscribe"
+                id="subscribe"
+                placeholder="Email Address"
+              />
+              <button
+                className="btn-primary"
+                onPointerMove={handleMouseOver}
+                onPointerLeave={({ target }) =>
+                  !target.disabled
+                    ? (target.style.background = "var(--dark-blue)")
+                    : null
+                }
+              >
+                Subscribe
+              </button>
+            </form>
+            <div className="close" onClick={() => setSubscribeOpen(false)}>
+              <FaTimes size={30} />
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </header>
   );
 }
